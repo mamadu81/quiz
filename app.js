0f3e0b8ba4f709359e464205a56301dbad869d09
mamadu81 <username@users.noreply.github.com>
2015-07-28 09:17:22 +0200

    Editar Preguntas

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
+var methodOverride = require('method-override');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -20,6 +22,7 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
