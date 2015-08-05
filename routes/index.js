b8bc6173fc648fa957e696a51ce892983151a7e5
mamadu81 <username@users.noreply.github.com>
2015-08-05 18:08:13 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 
@@ -13,6 +14,12 @@ router.get('/', function(req, res) {
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load); //autoload :quizId
 
+//Definición de rutas de sesión
+router.get('/login' ,sessionController.new);
+router.post('/login',sessionController.create);
+router.get('/logout',sessionController.destroy);
+
+//Definición de rutas de quizes
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
